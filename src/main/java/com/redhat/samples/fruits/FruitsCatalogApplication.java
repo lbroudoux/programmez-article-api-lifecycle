def8a29cdc42540fc816a2d235353e87dabe64ed
Laurent Broudoux <laurent.broudoux@gmail.com>
2021-09-14 16:09:44 +0200

    Adding more Javadoc comments

--- src/main/java/com/redhat/samples/fruits/FruitsCatalogApplication.java
+++ src/main/java/com/redhat/samples/fruits/FruitsCatalogApplication.java
@@ -2,6 +2,7 @@ package com.redhat.samples.fruits;
 
 /**
  * Fruits catalog application main class.
+ * Optional but allows starting up additional services or bootstrap initializations.
  * @author laurent
  */
 public class FruitsCatalogApplication {
